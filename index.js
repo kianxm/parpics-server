66b01f52388aa694b5bee511b67ec6ed9ad65bca
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-15 12:02:45 -0700

    feat: new packages

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+require("dotenv").config();
+
 const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
-
-const MONGO_DB =
-  "mongodb+srv://root:25yqEeWkDV1cyECA@parpics-db.1ctkv.mongodb.net/?retryWrites=true&w=majority&appName=parpics-db";
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({ typeDefs, resolvers });
+const port = parseInt(process.env.PORT, 10) || 8080;
 
 mongoose
-  .connect(MONGO_DB)
+  .connect(process.env.MONGO_DB)
   .then(() => {
     console.log("MongoDB connected");
-    return server.listen({ port: 5001 });
+    return server.listen({ port: port });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
