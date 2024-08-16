c4d2eaea390377038af10a8ce179da2f838fdf75
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-16 00:28:21 -0700

    feat: update client model

--- models/Client.js
+++ models/Client.js
@@ -3,13 +3,14 @@ const { model, Schema } = require("mongoose");
 const ClientSchema = new Schema({
   name: String,
   link: String,
-  accessCode: Number,
+  accessCode: String,
   location: String,
   date: String,
   hasPaid: Boolean,
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   photoCount: Number,
+  userId: { type: Schema.Types.ObjectId, ref: "User" },
 });
 
 module.exports = model("Client", ClientSchema);
