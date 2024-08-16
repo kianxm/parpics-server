3aa062924d49795be1be4a5c1d4c5fa22fc16514
Kian Malakooti <kianjmalakooti@gmail.com>
2024-08-16 00:28:29 -0700

    fix: mutations

--- graphql/resolvers/client.js
+++ graphql/resolvers/client.js
@@ -2,8 +2,8 @@ const Client = require("../../models/Client");
 
 module.exports = {
   Query: {
-    async getClient(_, { ID }) {
-      return await Client.findById(ID);
+    async getClient(_, { clientId }) {
+      return await Client.findById(clientId);
     },
     async getClients(_, { amount }) {
       return await Client.find().sort({ createdAt: -1 }).limit(amount);
@@ -11,11 +11,21 @@ module.exports = {
     async getAllClients() {
       return await Client.find().sort({ createdAt: -1 });
     },
+    async getAllClientsByUserId(_, { userId }) {
+      const clients = await Client.find({ userId }).sort({ createdAt: -1 });
+      return clients.map((client) => ({
+        id: client._id.toString(),
+        ...client._doc,
+      }));
+    },
   },
   Mutation: {
     async createClient(
       _,
-      { clientInput: { name, location, hasPaid, link, accessCode, date } }
+      {
+        clientInput: { name, location, hasPaid, link, accessCode, date },
+        userId,
+      }
     ) {
       const createdClient = new Client({
         name: name,
@@ -24,25 +34,33 @@ module.exports = {
         link: link,
         accessCode: accessCode,
         date: date,
+        userId: userId,
+        photoCount: 0,
       });
 
       const res = await createdClient.save();
 
       return {
-        id: res.id,
+        id: res._id.toString(),
         ...res._doc,
       };
     },
-    async deleteClient(_, { ID }) {
-      await Client.deleteOne(ID);
+    async deleteClient(_, { clientId }) {
+      const result = await Client.deleteOne({ _id: clientId });
+      if (result.deletedCount === 0) {
+        throw new Error("Client not found");
+      }
       return "Client deleted";
     },
     async editClient(
       _,
-      { ID, clientInput: { name, location, hasPaid, link, accessCode, date } }
+      {
+        clientId,
+        clientInput: { name, location, hasPaid, link, accessCode, date },
+      }
     ) {
       const updatedClient = await Client.updateOne(
-        { _id: ID },
+        { _id: clientId },
         { name, location, hasPaid, link, accessCode, date }
       );
       return updatedClient;
